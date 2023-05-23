2a01df7aefdda1e6c3c39953e32c06b0e8bac2d9
Hayley <hayleymdean271@gmail.com>
2023-05-23 16:59:31 -0700

    changed code

--- index.js
+++ index.js
@@ -1,6 +1,34 @@
-let greenCharacter = document.createElement('img')
-greenCharacter.src = 'assets/green-character.gif'
-greenCharacter.style.position = 'fixed'
-greenCharacter.style.left = '100px'
-greenCharacter.style.bottom = '100px'
-document.body.append(greenCharacter)
+function newImage(url, left, bottom) {
+    let image = document.createElement('img')
+    image.src = url
+    image.style.position = 'fixed'
+    image.style.left = left + 'px'
+    image.style.bottom = bottom + 'px'
+    document.body.append(image)
+    return image
+}
+
+function newItem(url, left, bottom){
+    let tool = newImage(url, left, bottom)
+    tool.src = url
+    tool.style.position = 'fixed'
+    tool.style.left = left + 'px'
+    tool.style.bottom = bottom + 'px'
+    document.body.append(tool)
+    return tool
+    
+    tool.addEventListener("dblclick", () => {
+        tool.remove()
+    })
+}
+
+newImage('assets/green-character.gif', 100, 100)
+newImage('assets/tree.png', 200, 300)
+newImage('assets/pillar.png', 350, 100)
+newImage('assets/pine-tree.png', 450, 200)
+newImage('assets/crate.png', 150, 200)
+newImage('assets/well.png', 500, 425)
+
+newItem('assets/sword.png', 500, 405)
+newItem('assets/shield.png', 165, 185)
+newItem('assets/staff.png', 600, 100)
